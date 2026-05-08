88ae59a1554dd2590775b197a57533b926b220d8
teannacottey <teannacottey@gmail.com>
2026-05-08 12:38:23 +0100

    2nd Commit: game functions created

--- TeannaOttey_Python_04_01_Project_GameDevelopment.py
+++ TeannaOttey_Python_04_01_Project_GameDevelopment.py
@@ -41,7 +41,7 @@ setting new conditions for each level"""
 for level_num, level in enumerate(levels, start=1): #enumerate creates index number for each level, level_num = index, level = dictionary 
     max_attempts = level["max_attempts"] #create variable to assign level max_attempt key's value 
     level_name = level["name"] #create variable to assign level name key's value
-    print(f"--- Level {level_num}: {level_name} | Maximum attempts: {max_attempts} ---\n")
+    print(f"--- Level {level_num}: {level_name} | Maximum attempts: {max_attempts} ---\n") #\n to create space between next line of text
 
 #--------------------------------------------------------------------------------------------------------------
 
@@ -53,6 +53,39 @@ timer_start = None #the timer hasn't started because the user has not rolled yet
 quit_game = False #allows game to continue unless user quits or exceeds maximum attempts 
 
 #--------------------------------------------------------------------------------------------------------------
-#VERSION CONTROL - COMMIT INITIAL CHANGES IN GIT
+
+#Create game functions 
+
+#create a function that checks snakes/ladders position 
+def check_square(position): #passes player position through function 
+    if position in snakes: #looks for player's landing (current) position - key 
+        print(f"| 🐍 Snake! Slide down to {snakes[position]} |", end = "") 
+        return snakes[position] # sends destination position (value) back to game loop, skips ladder check 
+    elif position in ladders: 
+        print(f"| 🪜 Ladder! Climb up to {ladders[position]} |", end = "")
+        return ladders[position]
+    return position #returns new position if no snakes or ladders encountered 
+
+#create a function that gets user number input 
+def get_roll(user_input, roll): 
+    while True: #create loop that prompts user input until valid
+        user_input = input("Roll the dice! 🎲 or enter a number (1-6).\n").strip() #prompts user to roll dice, enter number, or quit game
+        if user_input.lower() == 'q': #checks if user wants to quit the game 
+            return None #no number to return 
+        if user_input == "": #checks if users chose to roll the dice, i.e. pressed enter
+            roll = random.randint(1,6) #stores "rolled" number on dice 
+            print(f"| You rolled a {roll}!")
+            return roll #sends roll number back to game loop 
+        try: #validating user input 
+            roll = int(user_input) #convert user input to integer 
+            if 1<= roll <= 6: #checks input is within dice range 
+                return roll 
+            print("| ⚠️ Invalid input - Please enter a number between 1 and 6.\n") #sends error message if input out of range
+        except ValueError: #sends error message if input cannot be converted to an integer 
+            print("| ⚠️ Invalid input - Numbers only, please! (1-6). \n")
+
+#--------------------------------------------------------------------------------------------------------------
+
+#Create game loop 
 
 #Allow dice or number input for error handling purposes 

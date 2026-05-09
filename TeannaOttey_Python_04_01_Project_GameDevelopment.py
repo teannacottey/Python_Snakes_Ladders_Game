b4cbc6f6e1753f09f8d006b6e37e0333716d66c2
teannacottey <teannacottey@gmail.com>
2026-05-09 19:40:10 +0100

    5th Commit: additional debugging + game stats added

--- TeannaOttey_Python_04_01_Project_GameDevelopment.py
+++ TeannaOttey_Python_04_01_Project_GameDevelopment.py
@@ -3,6 +3,8 @@
 
 import random #allows code to create a dice to use in the game 
 import time #allows code to create a game timer 
+import json #allows code to save and load stats to/from a file 
+import os #allows code to check if stats file exists 
 
 #--------------------------------------------------------------------------------------------------------------
 #Creating the game board - incl snakes and ladders landing and destination positions 
@@ -23,7 +25,51 @@ levels = [
     {"name": "Hard", "max_attempts": 20}
 ]
 #--------------------------------------------------------------------------------------------------------------
+
+#Assign states file name 
+
+#stats_file = "game_stats.json"
+stats_file = os.path.join(os.path.expanduser("~"), "Desktop", "game_stats.json")
+
+#--------------------------------------------------------------------------------------------------------------
+
 #Create game functions 
+
+#create a function that loads stats from file or creates blank stats if the file not found 
+def load_stats(): 
+    if os.path.exists(stats_file): #checks if stats file already exists 
+        with open(stats_file, "r") as f: #opens file in read mode, assign alias 'f' 
+            return json.load(f) #loads and sends existing stats to game loop 
+    return { #returns blank stats if no file found 
+        "games_played": 0, 
+        "levels_passed": 0, 
+        "total_attempts": 0, 
+        "total_time": 0, 
+        "level_details": [] #stores a list of dictionaries for each level played 
+    }
+
+#create a function that automatically saves stats to file 
+def save_stats(game_stats):
+    with open(stats_file, "w") as f: #opens file in write mode & creates if it doesn't exist, assign alias 'f' 
+        json.dump(game_stats, f, indent=4) #converting game stats to json file 
+
+
+#create a function that prints game stats summary at the end of the game 
+def print_stats(game_stats, session_levels): 
+    print("\n------------------------------------------------ 📊 Game Statistics ------------------------------------------------\n")
+    print(f"Total games played: {game_stats['games_played']}")
+    print(f"Total levels passed: {game_stats['levels_passed']}")
+    print (f"Total attempts across all games: {game_stats['total_attempts']}")
+    print(f"Total time across all games: {round(game_stats['total_time'], 1)}")
+    print("-- This game's level breakdown --")
+    for level in session_levels: #shows current session level details 
+        if level["complete"]: 
+            status = "✅" 
+        else: 
+            status = "❌"
+        #takes appended stats from 'level_details' list in 'game_stats', referencing each level(dictionary) in the list 
+        print (f"{status} Level {level['level_num']} ({level['level_name']}): {level['attempts']} attempts | {level['time_taken']}s.") 
+
 
 #create a function that checks snakes/ladders position 
 def check_square(position): #passes player position through function, variable defined outside of function  
@@ -35,12 +81,11 @@ def check_square(position): #passes player position through function, variable d
         return ladders[position]
     return position #returns new position if no snakes or ladders encountered 
 
-"""replace snake/ladder block with - position = check_square(position)"""
 
 #create a function that gets user number input 
 def get_roll(): 
     while True: #create loop that prompts user input until valid
-        user_input = input("-- Roll the dice! 🎲 or enter a number (1-6). --\n").strip() #prompts user to roll dice, enter number, or quit game
+        user_input = input("\n-- Roll the dice! 🎲 or enter a number (1-6). --\n").strip() #prompts user to roll dice, enter number, or quit game
         if user_input.lower() == 'q': #checks if user wants to quit the game 
             return None #no number to return 
         if user_input == "": #checks if users chose to roll the dice, i.e. pressed enter
@@ -56,12 +101,23 @@ def get_roll():
             print("⚠️ Invalid input - Numbers only, please! (1-6).\n")
 
 #--------------------------------------------------------------------------------------------------------------
+
+#load stats automatically when game starts 
+
+#create variable to hold returned value of load_stats to be accessed and updated by rest of code
+game_stats = load_stats() 
+game_stats["games_played"] += 1 #increments games played count each time script is run 
+save_stats(game_stats) #passes game_stats through save_states function to record a new game session
+
+#--------------------------------------------------------------------------------------------------------------
+
 #Introduce game to user 
 
 print("------------------------------------------------ 🐍 Snakes and Ladders 🪜 ------------------------------------------------\n")                                          
 
 #Give user game instructions 
-print("----------------- Complete all 3 levels to win the game! -----------------\n \n -------------- Each level has fewer attemps so 👀... try to avoid the snakes 🐍, if you can! --------------\n")
+print("------------------------------------ Complete all 3 levels to win the game! ------------------------------------\n")
+print("\n -------------- Each level has fewer attemps so 👀... try to avoid the snakes 🐍, if you can! --------------\n")
 
 #instructions to start the game 
 print("-------- Ready to begin? 🤔 Press enter on your keyboard to roll the dice 🎲 or type 'q' to exit the game. --------\n")
@@ -69,66 +125,105 @@ print("-------- Ready to begin? 🤔 Press enter on your keyboard to roll the di
 #present level details to user 
 #create levels loop that allows users to pass through each level upon completing the level prior, setting new conditions for each level
 
+session_levels = [] #tracks only current session's level details 
+
 for level_num, level in enumerate(levels, start=1): #enumerate creates index number for each level, level_num = index, level = dictionary 
-
     max_attempts = level["max_attempts"] #create variable to assign level max_attempt key's value 
-    level_name = level["name"] #create variable to assign level name key's value
-
-    print(f"--- Level {level_num}: {level_name} | Maximum attempts: {max_attempts} ---\n") #\n to create space between next line of text
+    level_name = level["name"] #create variable to assign level name key's value 
+    print(f"\n--- Level {level_num}: {level_name} | Maximum attempts: {max_attempts} ---\n") #\n to create space between next line of text
 
 #--------------------------------------------------------------------------------------------------------------
 
-    #Set initial game state, inside for loop and level_complete loop to reset on each level/ level attempt   
-    position = 0 #board game starting position = off the board 
-    attempts = 0 #user have yet to roll the dice/input a number 
-    timer_start = None #the timer hasn't started because the user has not rolled yet 
-    quit_game = False #allows game to continue unless user quits or exceeds maximum attempts     
+    failed_level = True #initially set to true to enter while loop, break out when level passed
 
-     #Create games loop 
+    while failed_level: 
 
-    #allows a maximum position of 100, ends the game once reached
-    while position < 100: 
+        #Set initial game state, inside for loop and while loop to reset on each level/ level attempt   
+        position = 0 #board game starting position = off the board 
+        attempts = 0 #user have yet to roll the dice/input a number 
+        timer_start = None #the timer hasn't started because the user has not rolled yet 
+        quit_game = False #allows game to continue unless user quits or exceeds maximum attempts 
+        failed_level = False #allows game to contunue unless user fails a level     
 
-        #get user input 
-        roll = get_roll() #gets output from function 
-        if roll is None: #no value, player chose to quit the game. if false, moves on to next body of code
-            print("Goodbye 😢 Thanks for playing!\n")
-            quit_game = True 
-            break #exits game loop 
+        #Create games loop 
+
+        #allows a maximum position of 100, ends the game once reached
+        while position < 100: 
+
+            #get user input 
+            roll = get_roll() #gets output from function 
+            if roll is None: #no value, player chose to quit the game. if false, moves on to next body of code
+                print("Goodbye 😢 Thanks for playing!\n")
+                quit_game = True 
+                break #exits game loop 
+                
+            #starts time on first valid roll     
+            if timer_start is None: #checks if timer has started, ignores if false  
+                timer_start = time.time() #calculates start time of game in seconds 
+
+            #position and attempts check 
+            attempts += 1 #increases with each loop iteration 
+            prev_position = position #position prior to current loop iteration 
+            position = min(position + roll, 100) #new position after rolling the dice, position capped at 100 
+            print(f" You moved from {prev_position} → {position}\n")
+
+            #snakes and ladders check 
+            position = check_square(position) #calls function to check/reassign position accordingly 
+
+            #display final position after each attempt 
+            print(f" New Position: {position}\n")
+
+            #attempt limit check 
+            if attempts >= max_attempts: #checks if max attempts reached, ignores if false 
+                time_taken = round(time.time() - timer_start, 1) #calculates total time taken to reach attempt limit 
+                print(f"❌ Oh no! You have ran out of attempts | You reached position {position} in {time_taken}s.\n")
+                print(f"👾 Failed to complete Level {level_num}: {level_name} | Try Again!\n")
+
+                #record failed level stats and saves automatically 
+                game_stats["total_attempts"] += attempts
+                game_stats["total_time"] += time_taken 
+                level_entry = {
+                    "level_num": level_num, 
+                    "level_name": level_name, 
+                    "attempts": attempts, 
+                    "time_taken": time_taken, 
+                    "complete": False #level was failed 
+                }
+                game_stats["level_details"].append(level_entry)
+                session_levels.append(level_entry)
+                save_stats(game_stats)
+
+                failed_level = True #level failed, replays level 
+                break #exits level loop when atempts exceeded     
             
-        #starts time on first valid roll     
-        if timer_start is None: #checks if timer has started, ignores if false  
-            timer_start = time.time() #calculates start time of game in seconds 
-
-        #position and attempts check 
-        attempts += 1 #increases with each loop iteration 
-        prev_position = position #position prior to current loop iteration 
-        position = min(position + roll, 100) #new position after rolling the dice, position capped at 100 
-        print(f" You moved from {prev_position} → {position}\n")
-
-        #snakes and ladders check 
-        position = check_square(position) #calls function to check/reassign position accordingly 
-
-        #display final position after each attempt 
-        print(f" New Position: {position}\n")
-
-        #attempt limit check 
-        if attempts >= max_attempts: #checks if max attempts reached, ignores if false 
-            time_taken = round(time.time() - timer_start, 1) #calculates total time taken to reach attempt limit 
-            print(f"❌ Oh no! You have ran out of attempts | You reached position {position} in {time_taken}s.\n")
-            print(f"👾 Game Over | Failed to complete Level {level_num}: {level_name}\n")
-            quit_game = True
-            break #exits game loop when attempts exceeded     
-        
     if quit_game: #will be true if attempts exceeded or player quits, ignores if false  
-        break #exits level loop and ends the game 
+     break #exits level loop and ends the game 
 
     #User completes level, i.e. position exceeded 100 but didn't quit or exceed limits
     time_taken = round(time.time() - timer_start, 1)
     print(f"✅ Level {level_num} complete! You reached 100 in {attempts} attempts and {time_taken}s.\n")
 
-    #Checks if there are any levels left for the user to play 
+    game_stats["total_attempts"] += attempts
+    game_stats["total_time"] += time_taken 
+    level_entry = {
+        "level_num": level_num, 
+        "level_name": level_name, 
+        "attempts": attempts, 
+        "time_taken": time_taken, 
+         "complete": True #level was failed 
+        }
+    game_stats["level_details"].append(level_entry)
+    session_levels.append(level_entry)
+    save_stats(game_stats)
+
+#Checks if there are any levels left for the user to play 
     if level_num == len(levels): #number of levels
         print("🥳 Congratulations, you completed all levels - you're a Snakes and Ladder champion! 🏆")
     else: 
         print(f"Get ready for the next level!\n")    
+
+#--------------------------------------------------------------------------------------------------------------
+
+#Print stats summary at the end of the game 
+
+print_stats(game_stats, session_levels)
